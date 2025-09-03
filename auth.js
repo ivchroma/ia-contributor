88497d448ddbb3efd86d5aa0fdf9c4531a919e72
ivchroma <147657289+ivchroma@users.noreply.github.com>
2025-09-03 22:35:33 +0300

    Redirect
    Go back to your page where you belong

--- auth.js
+++ auth.js
@@ -1,37 +1,7 @@
-function parseJwt(token) {
-    const base64Url = token.split('.')[1]; // The payload is the 2nd part
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(
-        atob(base64)
-            .split('')
-            .map(c => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
-            .join('')
-    );
-    return JSON.parse(jsonPayload);
-}
+
 
 document.getElementById("mainUI").style.display = "block";
-/*function handleCredentialResponse(response) {
-    const userObject = parseJwt(response.credential);
-    document.getElementById("buttonDiv").style.display = "none";
-    document.getElementById("mainUI").style.display = "block";
-    document.getElementById("status").innerText = `${userObject.name}`;
-}
 
-// Initialize Google Sign-In
-google.accounts.id.initialize({
-    client_id: "115396371783-sc29nt2eefmk5o10ev18d5092vgqmrj0.apps.googleusercontent.com",
-    callback: handleCredentialResponse
-});
-
-// Render the sign-in button
-google.accounts.id.renderButton(
-    document.getElementById("buttonDiv"),
-    { theme: "outline", size: "large" }
-);
-google.accounts.id.prompt(); */
-
-// Create a single supabase client for interacting with your database
 
 
 const client = supabase.createClient('https://zzypezedfkegupwpwsam.supabase.co', 'sb_publishable_pasDUaq9bzG0kQkFIvyaeQ_XdvKTBO_')
@@ -41,7 +11,10 @@ const {data:{session}} = await client.auth.getSession()
 
 if(!session){
 const { data, error } = await client.auth.signInWithOAuth({
-  provider: 'google'
+  provider: 'google',
+  options: {
+    redirectTo: window.location.href
+  }
 })
 }
 else {
