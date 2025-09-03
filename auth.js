b9d1beea55edf2988799a010f0675d0a7ac06d43
ivchroma <147657289+ivchroma@users.noreply.github.com>
2025-09-03 15:46:44 +0300

    Why so query-ous?
    I'm the woker baby!

--- auth.js
+++ auth.js
@@ -10,7 +10,8 @@ function parseJwt(token) {
     return JSON.parse(jsonPayload);
 }
 
-function handleCredentialResponse(response) {
+document.getElementById("mainUI").style.display = "block";
+/*function handleCredentialResponse(response) {
     const userObject = parseJwt(response.credential);
     document.getElementById("buttonDiv").style.display = "none";
     document.getElementById("mainUI").style.display = "block";
@@ -28,19 +29,45 @@ google.accounts.id.renderButton(
     document.getElementById("buttonDiv"),
     { theme: "outline", size: "large" }
 );
-google.accounts.id.prompt();
+google.accounts.id.prompt(); */
 
 // Create a single supabase client for interacting with your database
 
+
 const client = supabase.createClient('https://zzypezedfkegupwpwsam.supabase.co', 'sb_publishable_pasDUaq9bzG0kQkFIvyaeQ_XdvKTBO_')
-async function qryID(){
-    const { data, error } = await client
-    .from('people')
-    .select('id')
-    if (error) {
-    console.error('Error:', error)
-    } else {
-    console.log('Data:', data)
-    }
+
+
+const {data:{session}} = await client.auth.getSession()
+
+if(!session){
+const { data, error } = await client.auth.signInWithOAuth({
+  provider: 'google'
+})
 }
+else {
+    console.log("User logged in:", session.user)
+    document.getElementById("buttonDiv").style.display = "none";
+    document.getElementById("mainUI").style.display = "block";
+    let username = session.user.user_metadata.full_name
+    document.getElementById("status").innerText = `${username}`;
+}
+
+
+function qryID() {
+  client
+    .from('people')
+    .select('person_id')
+    .eq('email', session.user.user_metadata.email)
+    .then(({ data, error }) => {
+      if (error) {
+        console.error('Error:', error);
+      } else {
+        console.log('Data:', data);
+        console.log(data[0].person_id);
+        return data[0].person_id
+      }
+    });
+}
+
 qryID()
+
